2568c89efdaec6897fcf8109deaaf95cfafd3903
Pape Soumaila Traoré <papejrtraore@gmail.com>
2025-01-31 15:24:57 +0000

    Ajout de la pied du page

---
+++ footer.php
@@ -0,0 +1,9 @@
+
+<footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+        <p>&copy; 2025 Kilakodon - Tous droits réservés.</p>
+    </footer>
+
+    
+</body>
+</html>
