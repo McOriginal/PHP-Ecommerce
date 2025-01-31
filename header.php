9fc961023d639a9bd5fbadc05933689202aab75d
Pape Soumaila Traoré <papejrtraore@gmail.com>
2025-01-31 15:29:24 +0000

    Conception Terminé pour la page d'accueil

--- header.php
+++ header.php
@@ -1,10 +1,66 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign Up</title>
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-</head>
+<?php 
+
+// Vérification du rôle d'utilisateur
+$user_logged_in = isset($_SESSION['user']);
+$user_role = $user_logged_in ? $_SESSION['user']['role'] : '';
+
+
+$page = $_GET['page'] ?? 'home';
+$titles = [
+    'home' => 'Bienvenue sur notre boutique',
+    'about' => 'A Propos de nous',
+    'addProduct' => 'Ajouter des produits',
+    'panier' => 'Votre Panier',
+    'login' => 'Se connecter'
+
+];
+$title = $titles[$page] ?? 'Boutique en ligne';
+
+require "_includes/_head.php"; 
+require_once "functions.php";
+
+?>
+
 <body>
+    
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <div class="navbar-image-top">
+            <a class="navbar-brand" href="logo">Kilakodon Sugu</a>
+        </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <?= nav_item("home", "Home") ?>
+                <?= nav_item("about", "A Propos") ?>
+                <?= nav_item("shop", "Boutique") ?>
+                
+                <?php if($user_logged_in): ?>
+                    <?= nav_item("panier", "Panier") ?>
+                    <?php if ($user_role === 'admin'): ?>
+                        <?= nav_item("addProduct", "Ajouter des produits") ?>
+                    <?php endif; ?>
+                    <?= nav_item("logout", "Se deconnecter") ?>
+
+
+                <?php else: ?>
+                    <?= nav_item("login", "Se connecter") ?>                    
+                <?php endif; ?>
+
+
+            </ul>
+            <div class="cart">
+                <i class="fas fa-shopping-cart"></i>
+                <span class="cart-count">0</span>&nbsp;&nbsp;
+            </div>
+
+            <!-- <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form> -->
+
+        </div>
+    </div>
+</nav>
--- pdo.php
+++
@@ -1,9 +0,0 @@
-<!-- Connexion à la base de données MySQL  -->
-
-
-<?php 
-
-    $pdo = new PDO('mysql:host=localhost;dbname=ecommerce','root','');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-
-?>
